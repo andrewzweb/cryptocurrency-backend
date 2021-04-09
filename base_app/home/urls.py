6d04e7b6a321975600c61256af13c68c46a2e93d
andrewzweb <andrewzweb@gmail.com>
2021-04-09 12:28:58 +0300

    Change stylesheet of project and add header and footer

--- base_app/home/urls.py
+++ base_app/home/urls.py
@@ -4,6 +4,6 @@ from . import views
 app_name='home'
 
 urlpatterns = [
-    path('home/', views.home, name='home'),
-    path('', views.landing, name='landing'),
+    path('', views.home, name='home'),
+    path('landing/', views.landing, name='landing'),
  ]

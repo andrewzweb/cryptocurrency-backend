ae40cb26e6a75e7bec6b34a77278403008662fdd
andrewzweb <andrewzweb@gmail.com>
2021-06-03 22:38:42 +0300

    Can deploy app use docker-compose

--- base_app/base_app/asgi.py
+++ base_app/base_app/asgi.py
@@ -1,11 +1,18 @@
 import os
+import sys
+from django.conf.urls import url
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'base_app.settings.__init__')
+
+import django
+django.setup()
+
 
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 import dashboard.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'base_app.settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
